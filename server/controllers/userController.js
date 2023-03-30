a49ebb8bced2d2022daa09f0774c37786c25bea0
Michael Byers <108441006+mike242424@users.noreply.github.com>
2023-03-30 06:02:48 -0400

    backend file structure, signup, and login routes

---
+++ server/controllers/userController.js
@@ -0,0 +1,38 @@
+const User = require('../models/userModel');
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find()
+    res.json({
+      users: users,
+    });
+  } catch (err) {
+    res.status(404).json({
+      message: err.message,
+    })
+  }
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    message: 'This route is not yet defined',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    message: 'This route is not yet defined',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    message: 'This route is not yet defined',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    message: 'This route is not yet defined',
+  });
+};
