255e34ada51da22b822aa50c992eacd4450f48c4
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2023-04-03 11:40:37 -0400

    Merge solution

---
+++ client/src/services/auth.service.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const API_URL = "http://localhost:8000/api/auth";
+
+const signup = (email, password) => {
+  return axios.post(API_URL + "signup", {
+    email,
+    password
+  });
+};
+
+const login = (email, password) => {
+  return axios.post(API_URL + "signin", {
+    email,
+    password
+  })
+  .then((response) => {
+    if (response.data.accessToken) {
+      localStorage.setItem("user", JSON.stringinfy(response.data));
+    }
+    return response.data;
+  });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+const authService = {
+  signup,
+  login,
+  logout,
+}
+
+export default authService;
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import boardsReducer from '../features/boardsSlice';
+import userAuthReducer from '../features/userAuthSlice';
+import authMessageReducer from '../features/authMessageSlice';
 
 
 export const store = configureStore({
   reducer: {
-    userBoards: boardsReducer
-  }
+    userBoards: boardsReducer,
+    userAuth: userAuthReducer,
+    authMessage: authMessageReducer
+  },
+  devTools: true,
 });
