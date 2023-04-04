641dc371024fd4e0cb02401a2813d43acf7c2a54
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2023-04-03 20:53:12 -0400

    top padding added SignUp and Login

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -13,23 +13,25 @@ const LoginForm = () => {
   }
 
   return (
-    <Card style={{ width: '18rem', marginRight: 'auto', marginLeft: 'auto' }} className="col=4 mt-7 mb=5 text-center">
-      <Card.Header>Login</Card.Header>
-      <Card.Body>
-        <Form onSubmit={handleSubmit(handleFormSubmit)}> 
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Enter Email" {...register("email")} />
-            <Form.Label>Email Address</Form.Label>
-          </Form.Group>
+    <div className="pt-5">
+      <Card style={{ width: '18rem', marginRight: 'auto', marginLeft: 'auto' }} className="col=4 text-center">
+        <Card.Header>Login</Card.Header>
+        <Card.Body>
+          <Form onSubmit={handleSubmit(handleFormSubmit)}> 
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Enter Email" {...register("email")} />
+              <Form.Label>Email Address</Form.Label>
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" {...register("password")} />
-            <Form.Label>Password</Form.Label>
-          </Form.Group>
-          <Button variant="primary" type="submit">Submit</Button>
-        </Form>
-      </Card.Body>
-    </Card>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Password" {...register("password")} />
+              <Form.Label>Password</Form.Label>
+            </Form.Group>
+            <Button variant="primary" type="submit">Submit</Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
 
--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -14,28 +14,30 @@ const SignUpForm = () => {
   }
 
   return (
-    <Card style={{ width: '18rem', marginRight: 'auto', marginLeft: 'auto' }} className="col=4 mt-7 mb=5 text-center">
-      <Card.Header>Sign Up</Card.Header>
-      <Card.Body>
-        <Form onSubmit={handleSubmit(handleFormSubmit)}> 
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Enter Email" {...register("email")} />
-            <Form.Label>Email Address</Form.Label>
-          </Form.Group>
+    <div className="pt-5">
+      <Card style={{ width: '18rem', marginRight: 'auto', marginLeft: 'auto' }} className="col=4  text-center">
+        <Card.Header>Sign Up</Card.Header>
+        <Card.Body>
+          <Form onSubmit={handleSubmit(handleFormSubmit)}> 
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Enter Email" {...register("email")} />
+              <Form.Label>Email Address</Form.Label>
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" {...register("password")} />
-            <Form.Label>Password</Form.Label>
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Password" {...register("password")} />
+              <Form.Label>Password</Form.Label>
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicOrganization">
-            <Form.Control type="text" placeholder="Organization" {...register("organization")} />
-            <Form.Label>Organization</Form.Label>
-          </Form.Group>
-          <Button variant="primary" type="submit">Submit</Button>
-        </Form>
-      </Card.Body>
-    </Card>
+            <Form.Group className="mb-3" controlId="formBasicOrganization">
+              <Form.Control type="text" placeholder="Organization" {...register("organization")} />
+              <Form.Label>Organization</Form.Label>
+            </Form.Group>
+            <Button variant="primary" type="submit">Submit</Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
 
