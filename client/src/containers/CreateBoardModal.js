22c1eccb00dbda551e76a1fe14a10a259f576169
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2023-04-06 15:56:11 -0400

    CreateBoardModal refactored to match Modal in BoardView. Styling needs to be adjusted.

---
+++ client/src/containers/CreateBoardModal.js
@@ -0,0 +1,24 @@
+import AddBoardForm from "./AddBoardForm";
+import ReactModal from "react-modal";
+import { useDispatch, useSelector } from "react-redux";
+import { setModalClosed } from "../features/modalOpenSlice";
+import { Modal } from "react-bootstrap";
+
+  
+const CreateBoardModal = () => {
+  const isOpen = useSelector((state) => state.isModalOpen.open);
+
+  const dispatch = useDispatch();
+
+  const handleModalClose = () => {
+    dispatch(setModalClosed());
+  }
+  return (
+    <Modal show={isOpen} onHide={handleModalClose}>
+    <Modal.Header closeButton>Create Board</Modal.Header>
+      <AddBoardForm />
+    </Modal>
+  )
+}
+
+export default CreateBoardModal;
