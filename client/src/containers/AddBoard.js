a18e8a8c72b34c5aef3a72ca35fccec15c18c0aa
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2023-04-06 14:32:29 -0400

    Styled Modal. Added a button to close Modal.

--- client/src/containers/AddBoard.js
+++ client/src/containers/AddBoard.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import { useDispatch, useSelector } from "react-redux";
 import ReactModal from "react-modal";
 import AddBoardForm from "./AddBoardForm";
-import { setModalOpen } from "../features/modalOpenSlice";
+import { setModalOpen, setModalClosed } from "../features/modalOpenSlice";
 
 const AddBoard = () => {
   const dispatch = useDispatch();
@@ -13,13 +13,30 @@ const AddBoard = () => {
     dispatch(setModalOpen());
   }
 
+  const handleButtonClick = () => {
+    dispatch(setModalClosed());
+  }
+
   return (
     <Col sm={4}>
       <Button onClick={handleClick}>+ Add New Board</Button>
       <ReactModal
         isOpen={isOpen}
         ariaHideApp={false}
-        contentLabel="Board Title">
+        contentLabel="Board Title"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.75)'
+          },
+          content: {
+            width: '30%',
+            height: '50%',
+            margin: 'auto',
+            borderRadius: '10px',
+            boxShadow: '0px 0px 10px 2px rgba(0, 0, 0, 0.25)'
+          }
+        }}>
+          <Button onClick={handleButtonClick}>X</Button>
           <AddBoardForm />
         </ReactModal>
     </Col>
