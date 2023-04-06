7cff77a93fd7d198b31a64e5222c0e013353b153
Michael Byers <108441006+mike242424@users.noreply.github.com>
2023-04-06 05:19:40 -0400

    minor changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,12 +2,18 @@ const User = require('../models/userModel');
 
 exports.getAllUsers = async (req, res) => {
   try {
-    const users = await User.find()
-    res.json({
-      users: users,
-    });
+    const users = await User.find();
+    if (!users) {
+      return res
+          .status(404)
+          .json({ message: 'No users found' });
+    } else {
+      res.status(200).json({
+        users: users,
+      });
+    }
   } catch (err) {
-    res.status(404).json({
+    res.status(500).json({
       message: err.message,
     })
   }
